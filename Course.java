8c737f919b9f355f786a633f925fdf255b703017
Tanya <TanyaSelvog@gmail.com>
2021-08-10 14:03:18 -0700

    added Course and CourseDemo
    From 8.4 Aggregation

---
+++ Course.java
@@ -0,0 +1,84 @@
+/**
+This class stores data about a course.
+*/
+
+public class Course
+{
+    private String courseName; // Name of the course
+    private Instructor instructor; // Instructor
+    private TextBook textBook; // Textbook
+
+    /**
+    This constructor initializes the courseName,
+    instructor, and text fields.
+    @param name Name of course
+    @param instructor An Instructor object
+    @param text A TextBook object
+    */
+
+    public Course(String name, Instructor instr,
+    TextBook text)
+    {
+        // Assign the courseName
+        courseName = name;
+
+        // Create a new Instructor object, passing
+        // instr as an argument to the copy constructor
+        instructor = new Instructor(instr);
+
+        // Create a new TextBook object, passing
+        // text as an argument to the copy constructor
+        textBook = new TextBook(text);
+    }
+
+    /**getName method
+    @return Name of course
+    */
+
+    public String getName()
+    {
+        return courseName;
+    }
+
+    /**
+    getInstructor method
+    @return A reference to a copy of this course's
+    Instructor object
+    */
+
+    public Instructor getInstructor()
+    {
+        // Return a copy of the instructor object
+        return new Instructor(instructor);
+    }
+
+    /**
+    getTextBook method
+    @return A reference to a copy of this course's
+    TextBook object
+    */
+
+    public TextBook getTextBook()
+    {
+        // Return a copy of the textBook object
+        return new TextBook(textBook);
+    }
+
+    /**
+    toString method
+    @return A string containing the course info
+    */
+
+    public String toString()
+    {
+        // Create a string representing the object
+        String str = "Course name: " + courseName +
+                     "\nInstructor Information:\n" +
+                     instructor +
+                     "\nTextbook Information:\n" +
+                     textBook;
+        
+        // Return the string
+        return str;
+    }
+}
